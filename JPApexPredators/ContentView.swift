ec5ba3d172b73f827010b00b061abe3d946078db
ishaan19d <ishaandas848@gmail.com>
2024-06-11 16:47:42 +0530

    "Initial Commit: Search functionality is built"

--- JPApexPredators/ContentView.swift
+++ JPApexPredators/ContentView.swift
@@ -8,14 +8,58 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    
+    let predators = Predators()
+    
+    @State var searchText: String = ""
+    
+    var filteredDinos: [ApexPredator] {
+        if searchText.isEmpty {
+            return predators.apexPredators
         }
-        .padding()
+        else{
+            return predators.apexPredators.filter { predator in
+                predator.name.localizedStandardContains(searchText)
+            }
+        }
+    }
+    
+    var body: some View {
+        NavigationStack{
+            List(filteredDinos) { predator in
+                NavigationLink {
+                    Image(predator.image)
+                        .resizable()
+                        .scaledToFit()
+                }label : {
+                    HStack{
+                        Image(predator.image)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 100, height: 100)
+                            .padding(.trailing)
+                        
+                        VStack(alignment: .leading){
+                            Text(predator.name)
+                                .fontWeight(.bold)
+                            
+                            Text(predator.type.rawValue.capitalized)
+                                .fontWeight(.semibold)
+                                .padding(.horizontal, 13)
+                                .padding(.vertical, 5)
+                                .background(predator.type.background)
+                                .clipShape(.capsule)
+                                .foregroundStyle(.white)
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Apex Predators")
+            .searchable(text: $searchText)
+            .autocorrectionDisabled()
+            .animation(.default, value: searchText)
+        }
+        .preferredColorScheme(.dark)
     }
 }
 
