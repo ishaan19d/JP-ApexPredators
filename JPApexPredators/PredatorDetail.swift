d56d94afc9ac5623ea7672831c13f04a9f1967e4
ishaan19d <ishaandas848@gmail.com>
2024-06-12 19:23:51 +0530

    Added: AccentColor and Gradient

--- JPApexPredators/PredatorDetail.swift
+++ JPApexPredators/PredatorDetail.swift
@@ -17,6 +17,9 @@ struct PredatorDetail: View {
                     Image(predator.type.rawValue)
                         .resizable()
                         .scaledToFit()
+                        .overlay {
+                            LinearGradient(stops: [Gradient.Stop(color: .clear, location: 0.5), Gradient.Stop(color: .black, location: 1)],startPoint: .top, endPoint: .bottom)
+                        }
                     
                     Image(predator.image)
                         .resizable()
@@ -73,6 +76,6 @@ struct PredatorDetail: View {
 }
 
 #Preview {
-    PredatorDetail(predator: Predators().apexPredators[2])
-        .preferredColorScheme(.dark)
+    PredatorDetail(predator: Predators().apexPredators[7])
+//        .preferredColorScheme(.dark)
 }
